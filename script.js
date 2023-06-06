034ba46f1e9c95b920f7f42982dc7cde68298101
JulienGB6MMb <113606827+JulienGB6MMb@users.noreply.github.com>
2023-06-06 09:07:33 +0200

    Push

---
+++ script.js
@@ -0,0 +1,46 @@
+function openModal(imgElement) {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("modalImage");
+    modal.style.display = "block";
+    modalImg.src = imgElement.src;
+  }
+  
+  function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+  }
+
+// Voeg klikgebeurtenis toe aan de afbeeldingen
+const images = document.querySelectorAll('.gallery img');
+images.forEach((image) => {
+	image.addEventListener('click', () => {
+		const src = image.src;
+		openModal(src);
+	});
+});
+
+// Voeg klikgebeurtenis toe aan de close-knop
+const closeBtn = document.querySelector('.close-btn');
+closeBtn.addEventListener('click', closeModal);
+
+// Sluit de modale weergave wanneer er buiten de afbeelding wordt geklikt
+window.addEventListener('click', (event) => {
+	const modal = document.querySelector('.modal');
+	if (event.target === modal) {
+		closeModal();
+	}
+});
+
+
+
+document.getElementById('navbarToggler').addEventListener('click', function() {
+    this.classList.toggle('open');
+    document.getElementById('navbarNav').classList.toggle('open');
+  });
+  
+  $(document).ready(function(){
+    $('.container ul.toggle').click(function(){
+        $(this).toggleClass('active');
+        $('.container .sidebar').toggleClass('active');
+    })
+})
